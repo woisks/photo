66c814b77ef545b29f109bf272315df2e3fc5f7f
woisks <woisks@126.com>
2019-06-12 17:19:18 +0800

    photo init

--- src/Models/Services/PhotoHelpers.php
+++ src/Models/Services/PhotoHelpers.php
@@ -17,13 +17,35 @@ namespace Woisks\Photo\Models\Services;
 
 use Woisks\Photo\Models\Entity\PhotoEntity;
 
+/**
+ * Class PhotoHelpers.
+ *
+ * @package Woisks\Photo\Models\Services
+ *
+ * @Author  Maple Grove  <bolelin@126.com> 2019/6/12 17:18
+ */
 class PhotoHelpers
 {
+    /**
+     * exists. 2019/6/12 17:18.
+     *
+     * @param int $id
+     *
+     * @return mixed
+     */
     public static function exists(int $id)
     {
         return PhotoEntity::where('id', $id)->exists();
     }
 
+    /**
+     * transUrl. 2019/6/12 17:18.
+     *
+     * @param $type
+     * @param $id
+     *
+     * @return array|string
+     */
     public static function transUrl($type, $id)
     {
         if (is_array($id)) {
